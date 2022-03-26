d4570ddaf33e301cf9d5d9fc937da722de1d8372
Badou <bryan.fiarda@outlook.com>
2022-03-27 00:30:19 +0100

    Tutorials Rust - Day 1

---
+++ src/print.rs
@@ -0,0 +1,18 @@
+pub fn run() {
+
+    // Print to console
+
+    println!("Hello From the print.rs file");
+
+    // Basic Formatting
+
+    println!("{} is from {}", "Maygane", "Baden");
+
+    // Positional Arguments
+
+    println!("{0} is from {1} and {0} likes to {2}", "Baden", "Mass", "Code");
+
+    // Named Arguments
+
+    println!("{name} likes to play {activity}", name = "Maygane", activity = "Cosplayer");
+}
